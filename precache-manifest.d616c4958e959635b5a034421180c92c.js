b88d066b3a071bd4de0a66b85fa714f5d7697b66
mohamedSamyAli <msamy2019@gmail.com>
2020-02-08 21:02:04 +0200

    Updates

---
+++ precache-manifest.d616c4958e959635b5a034421180c92c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d582a056379b232139ce8635e77986",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "b076fd493f43fbb8b6f0",
+    "url": "/weatherApp/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "7d850058acb81615ccd3",
+    "url": "/weatherApp/static/css/main.53d1142f.chunk.css"
+  },
+  {
+    "revision": "b076fd493f43fbb8b6f0",
+    "url": "/weatherApp/static/js/2.edd0e55c.chunk.js"
+  },
+  {
+    "revision": "c88e840375c6347b64d049b61c1560fe",
+    "url": "/weatherApp/static/js/2.edd0e55c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d850058acb81615ccd3",
+    "url": "/weatherApp/static/js/main.b847a3ea.chunk.js"
+  },
+  {
+    "revision": "2f7c040d631c675aa39b",
+    "url": "/weatherApp/static/js/runtime-main.67387127.js"
+  }
+]);
